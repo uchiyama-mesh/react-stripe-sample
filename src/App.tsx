150aacbaac096e0037c38906e515327061988365
uchiyama-mesh <s.uchiyama@smesh.jp>
2022-06-08 16:01:45 +0900

    feat: Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 
-export const App = () => <Home />
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+)
